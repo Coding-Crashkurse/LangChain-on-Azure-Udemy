ce1067e6645ac5f9b5eeee44e573ca99c40ef020
markus <datamastery87@gmail.com>
2023-11-30 20:52:32 +0100

    update

--- application/backend/app.py
+++ application/backend/app.py
@@ -8,11 +8,41 @@ import logging
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.pgvector import PGVector
 from langchain.schema import StrOutputParser, RunnablePassthrough
+from fastapi import FastAPI, UploadFile, File
+from azure.storage.blob import BlobServiceClient
+import os
+from fastapi import FastAPI, HTTPException
+from langchain.schema import Document
+from langchain.indexes import SQLRecordManager, index, IndexingResult
+
 
 load_dotenv(find_dotenv())
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+conn_str = os.getenv("BLOB_CONN_STRING")
+container_name = os.getenv("BLOB_CONTAINER")
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=conn_str)
+
+host = os.getenv("PG_VECTOR_HOST")
+user = os.getenv("PG_VECTOR_USER")
+password = os.getenv("PG_VECTOR_PASSWORD")
+COLLECTION_NAME = os.getenv("PG_COLLECTION_NAME")
+CONNECTION_STRING = (
+    f"postgresql+psycopg2://{user}:{password}@{host}:5432/{COLLECTION_NAME}"
+)
+
+namespace = f"pgvector/{COLLECTION_NAME}"
+record_manager = SQLRecordManager(namespace, db_url=CONNECTION_STRING)
+
+embeddings = OpenAIEmbeddings()
+
+vector_store = PGVector(
+    embedding_function=embeddings,
+    collection_name=COLLECTION_NAME,
+    connection_string=CONNECTION_STRING,
+)
 
 
 class Message(BaseModel):
@@ -80,3 +110,33 @@ async def ask_question(question: str, conversation: Conversation) -> dict:
     history_formatted = format_history(conversation)
     answer = rag_chain.invoke({"question": question, "history": history_formatted})
     return {"answer": answer}
+
+
+@app.post("/uploadfiles/")
+async def upload_files(files: list[UploadFile] = File(...)):
+    container_client = blob_service_client.get_container_client(container_name)
+    uploaded_files = []
+
+    for file in files:
+        blob_client = container_client.get_blob_client(blob=file.filename)
+
+        contents = await file.read()
+        blob_client.upload_blob(contents, overwrite=True)
+        uploaded_files.append(file.filename)
+
+    return {"uploaded_files": uploaded_files}
+
+
+@app.post("/index_documents/", response_model=IndexingResult)
+async def index_documents(documents: list[Document]) -> IndexingResult:
+    try:
+        result = index(
+            documents,
+            record_manager,
+            vector_store,
+            cleanup="full",
+            source_id_key="source",
+        )
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
